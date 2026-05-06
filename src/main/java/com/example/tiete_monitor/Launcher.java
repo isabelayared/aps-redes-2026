cd1e84c836eef8b04e929c343deeb09e7cc4d16c
Isabela Yared Soares <167128156+isabelayared@users.noreply.github.com>
2026-05-06 12:46:01 -0300

    Update Launcher.java

--- src/main/java/com/example/tiete_monitor/Launcher.java
+++ src/main/java/com/example/tiete_monitor/Launcher.java
@@ -1,9 +1,19 @@
 package com.example.tiete_monitor;
 
 import javafx.application.Application;
+import com.example.tiete_monitor.frontend.MainApp;
 
+/**
+ * CLASSE LAUNCHER (Inicializador)
+ * ---------------------------------------------------------
+ * O JavaFX exige que a classe principal (que herda de Application)
+ * seja carregada corretamente pelo motor da máquina virtual.
+ * Esta classe isolada garante que o método Application.launch()
+ * seja executado sem erros de carregamento de módulos.
+ */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        // Aponta para a classe MainApp, que contém as telas do nosso Front-end.
+        Application.launch(MainApp.class, args);
     }
 }
