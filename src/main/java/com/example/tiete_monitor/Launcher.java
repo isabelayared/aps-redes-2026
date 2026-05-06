040fe41b79a75b401155a777caafb310b8d03fa3
Isabela Yared Soares <167128156+isabelayared@users.noreply.github.com>
2026-05-06 13:30:20 -0300

    Update Launcher.java

--- src/main/java/com/example/tiete_monitor/Launcher.java
+++ src/main/java/com/example/tiete_monitor/Launcher.java
@@ -1,9 +1,10 @@
 package com.example.tiete_monitor;
 
 import javafx.application.Application;
+import com.example.tiete_monitor.frontend.MainApp;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(MainApp.class, args);
     }
 }
